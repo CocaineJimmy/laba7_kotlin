15f17432d46f402425620b002f0a31201232e5e1
sadar <sadarqsa@gmail.com>
2024-03-21 22:53:05 +0300

    Update Theme.kt

--- app/src/main/java/com/topic2/android/notes/theme/Theme.kt
+++ app/src/main/java/com/topic2/android/notes/theme/Theme.kt
@@ -2,11 +2,13 @@ package com.topic2.android.notes.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 
 private val LightThemeColors = lightColors(
   primary = rwGreen,
@@ -37,3 +39,10 @@ fun NotesTheme(content: @Composable () -> Unit) {
 object NotesThemeSettings {
   var isDarkThemeEnabled by mutableStateOf(false)
 }
+
+private val DarkThemeColors = darkColors(
+  primary = Color(0xFF00A055),
+  primaryVariant = Color(0xFF00F884),
+  secondary = rwRed,
+  onPrimary = Color.White
+)
