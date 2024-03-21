338c6c7511551b57a565010cefadecd93899f104
sadar <sadarqsa@gmail.com>
2024-03-21 23:03:21 +0300

    Update TopAppBar.kt

--- app/src/main/java/com/topic2/android/notes/ui/components/TopAppBar.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/TopAppBar.kt
@@ -1,5 +1,3 @@
-package com.topic2.android.notes.ui.components
-
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -22,7 +20,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.topic2.android.notes.theme.NotesTheme
 
 @Composable
 fun TopAppBar(
